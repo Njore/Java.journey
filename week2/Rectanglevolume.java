1347c30b96337126691a511dbfec9870eeabc41d
Njore <eve.n.njore@gmail.com>
2024-01-27 09:42:30 +0300

    Rec Vol without delimiter

---
+++ week2/Rectanglevolume.java
@@ -0,0 +1,9 @@
+public class Rectanglevolume{
+    public static void main(String[] args){
+        int vol = 200000;
+        short l = 50;
+        short w = 32;
+        int h = vol/(l*w);
+        System.out.println("The height of the rectangle is, \t" + h);
+    }
+}
