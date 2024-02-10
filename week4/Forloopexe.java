28a766f531f4a753951d8e436ae3f5797ddb0116
Njore <eve.n.njore@gmail.com>
2024-02-10 19:11:11 +0300

    nested forloops

---
+++ week4/Forloopexe.java
@@ -0,0 +1,18 @@
+public class Forloopexe{
+    public static void main(String [] args){
+//simple fo loop exe
+        for(int i=1;
+            i<=10;i++){
+            System.out.println(i);}
+//nested loops
+        aa:
+        for(int i=1;i<=3;i++){
+            bb:
+            for(int j=1; j<=3;j++){
+                if(i==2 && j==2){
+                    break aa;}
+                System.out.println(i+ "   " +j);
+            }
+        }
+    }
+}
