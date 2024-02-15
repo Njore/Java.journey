57808a89170cda558546c45831ed9929a2f9aeda
Njore <eve.n.njore@gmail.com>
2024-02-15 22:24:19 +0300

    understanding methods

---
+++ week4/methods.java
@@ -0,0 +1,8 @@
+/*
+modifier: defines the access type of the method and it's optional to use.
+returnType: Method may return a value.
+nameOfMethod: This is the method name. The method signature consists of the method name
+and the parameter list.
+Parameter List: The list of parameters, it is the type, order, and number of parameters of a
+method. These are optional, method may contain zero parameters.
+ method body: The method body defines what the method does with statements*/
